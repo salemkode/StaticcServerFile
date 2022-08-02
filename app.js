944a7c940655b39debb14f4eb3c3e7bc5e99158b
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2022-08-02 07:19:47 +0300

    Add list of file in home page

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 //
 require('dotenv').config()
+const fs = require("fs");
+const path = require('path');
 
 //
 const express = require("express"),
   app = express();
+
+//
+app.get("/", async (req, res) => {
+  const _path = path.join(__dirname, "static");
+  fs.readdir(_path, (error, items) => {
+    if (error) {
+      res.send({
+        error: true,
+        message: "I can't get files list"
+      });
+    } else {
+      const result = items.map((item) => {
+        if (!item.startsWith(".")) {
+          return `<a href="${req.protocol}://${path.join(req.get("host"), item)}">${item}</a> <br />`;
+        }
+      })
+
+      res.send(result.join(""));
+    }
+  })
+});
 
 //
 app.use(express.static("./static"));
